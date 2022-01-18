ef9b9a42ecf7386521c4106e322fe0c66ae05100
alex-udodik <alex.udodik@wsu.edu>
2022-01-17 23:02:19 -0800

    Added slash command: add

--- index.js
+++ index.js
@@ -9,6 +9,37 @@ const client = new DiscordJS.Client({
 
 client.on('ready', () => {
     console.log("The bot is ready.");
+
+    const guildId = process.env.GUILD_ID;
+    const guild = client.guilds.cache.get(guildId);
+
+    let commands
+    
+    if (guild) {
+        commands = guild.commands
+    } else {
+        commands = client.application?.commands
+    }
+
+    commands?.create({
+        name: 'add',
+        description: 'Subscribes a Twitch channel for live notifications.',
+    })
+})
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommands()) {
+        return;
+    }
+
+    const { commandName, options } = interaction
+
+    if (commandName === 'add') {
+        interaction.reply({
+            content: "test",
+            ephemeral: true,
+        })
+    }
 })
 
 client.login(process.env.BOT_TOKEN);
